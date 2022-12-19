f9a6a53c6261b56112e641f4f48e565adc9b152a
levch <levchenko.d.a1998@gmail.com>
2022-12-20 04:54:26 +0600

    v 0.11

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ if [ ! -f $ENV_FILE_NAME ]; then
   return
 fi
 
-docker build . -t $APP_NAME
-docker run -ti $APP_NAME --restart=always --name=$APP_NAME -e TZ=Europe/Moscow
+docker build -t $APP_NAME .
+docker run -ti --restart=always --name=$APP_NAME -e TZ=Europe/Moscow $APP_NAME
 
 #docker compose up --build -d
